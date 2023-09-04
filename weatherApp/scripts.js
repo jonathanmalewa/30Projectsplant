542ae4e7b848adf59aa22bee5e39932c68aa889e
Jonathan Malewa <139697835+jonathanmalewa@users.noreply.github.com>
2023-09-04 16:33:19 +0800

    WeatherApp
    WeatherApp Menggunakan HTML, CSS dan Javascript

---
+++ weatherApp/scripts.js
@@ -0,0 +1,46 @@
+const apiKey = "0e1e5a1279bc6e3714d403dd6942e6cc";
+const apiUrl =
+  "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+//mengambil query di html sehingga dapat dipanggil
+const searchBox = document.querySelector(".search input");
+const searcBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
+
+async function checkWeather(city) {
+  const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+  // Kondisi pada saat mencari kota yang tidak ada
+  if (response.status == 404) {
+    document.querySelector(".error").style.display = "block";
+    document.querySelector(".weather").style.display = "none";
+  } else {
+    var data = await response.json();
+
+    // Mengambil data
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML =
+      Math.round(data.main.temp) + "°C";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + " Km/H";
+
+    if (data.weather[0].main == "Clouds") {
+      weatherIcon.src = "images/clouds.png";
+    } else if (data.weather[0].main == "Clear") {
+      weatherIcon.src = "images/clear.png";
+    } else if (data.weather[0].main == "Drizzle") {
+      weatherIcon.src = "images/drizzle.png";
+    } else if (data.weather[0].main == "Rain") {
+      weatherIcon.src = "images/rain.png";
+    } else if (data.weather[0].main == "Mist") {
+      weatherIcon.src = "images/mist.png";
+    }
+
+    document.querySelector(".weather").style.display = "block";
+    document.querySelector(".error").style.display = "none";
+  }
+}
+
+// pada saat button di klik maka akan mengambil data dari checkWeather dengan searchbox sebagai nilai inputnya
+searcBtn.addEventListener("click", () => {
+  checkWeather(searchBox.value);
+});
